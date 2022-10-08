a660ca71108b0056f4df4c18b63b548489bb2e1b
Сучков Алексей <AlexiySV@yandex.ru>
2022-10-08 13:29:09 +0300

    Task_21

---
+++ task_21/Program.cs
@@ -0,0 +1,17 @@
+﻿double GetData(string message)
+{
+    Console.Write(message);
+    double input = double.Parse(Console.ReadLine());
+    return input;
+}
+
+int i = 0;
+double Sum = 0;
+double[] A = { GetData("введите координату x точки A x="), GetData("введите координату y точки A y="), GetData("введите координату z точки A z=") };
+double[] B = { GetData("введите координату x точки B x="), GetData("введите координату y точки B y="), GetData("введите координату z точки B z=") };
+while (i < A.Length)
+{
+    Sum = Sum + (Math.Pow(A[i] - B[i], 2));
+    i++;
+}
+Console.WriteLine(Math.Sqrt(Sum));
