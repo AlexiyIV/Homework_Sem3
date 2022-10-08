9cde382c7ec22be8542b2c03ede8491902c27350
Сучков Алексей <AlexiySV@yandex.ru>
2022-10-08 14:30:15 +0300

    Task_23

---
+++ task_23/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите N=");
+int N = int.Parse(Console.ReadLine());
+int[] A = new int[Math.Abs(N)];
+if (N == 0)
+{
+    Console.WriteLine("Ошибка");
+}
+else if (N > 0)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = (int)Math.Pow(i + 1, 3);
+        Console.Write(A[i] + " ");
+    }
+}
+else
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = (int)Math.Pow(N, 3);
+        N = N + 1;
+        Console.Write(A[i] + " ");
+    }
+
+}
