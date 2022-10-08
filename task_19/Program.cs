58a9d6fa0a3a1ed77c1b7638285c8cbea731937f
Сучков Алексей <AlexiySV@yandex.ru>
2022-10-08 16:14:58 +0300

    Task_19

---
+++ task_19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число A=");
+int A = Math.Abs(int.Parse(Console.ReadLine()));
+
+bool IsPalindrom(int A)
+{
+    string n = A.ToString();
+    for (int i = 0; i < n.Length / 2; i++)
+    {
+        if (n[i] != n[n.Length - 1 - i]) return false;
+    }
+    return true;
+}
+Console.WriteLine(IsPalindrom(A));
